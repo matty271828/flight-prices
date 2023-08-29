8985bf4c3dd623e4a8ac196c3f3b2af59acbaf81
MATTHEW MacLean <mrm109@cs.bham.ac.uk>
2023-08-29 23:18:23 +0100

    Retrieve path from running binary

--- main.go
+++ main.go
@@ -4,8 +4,8 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 	"path/filepath"
-	"runtime"
 
 	"github.com/matty271828/flight-prices/amadeus"
 	"github.com/matty271828/flight-prices/controller"
@@ -13,8 +13,12 @@ import (
 )
 
 func main() {
-	_, b, _, _ := runtime.Caller(0)
-	basepath := filepath.Dir(b)
+	// Get the path of the currently running executable
+	execPath, err := os.Executable()
+	if err != nil {
+		log.Fatalf("Error determining executable path: %s\n", err)
+	}
+	basepath := filepath.Dir(execPath)
 
 	amadeusClient, err := amadeus.NewAmadeusClient()
 	if err != nil {
@@ -29,7 +33,9 @@ func main() {
 	mux := http.NewServeMux()
 	mux.Handle("/api/", http.StripPrefix("/api", s))
 
-	fs := http.FileServer(http.Dir(filepath.Join(basepath, "ui")))
+	uiPath := filepath.Join(basepath, "ui")
+	log.Printf("Serving UI from: %s", uiPath) // Log the UI path for debugging
+	fs := http.FileServer(http.Dir(uiPath))
 	mux.Handle("/", fs)
 
 	// Start the server
