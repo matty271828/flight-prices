15c2e2a28e7485f36f89e1e639a54b863a93d619
MATTHEW MacLean <mrm109@cs.bham.ac.uk>
2023-08-30 00:06:53 +0100

    Add local deploy bash file to gitignore

---
+++ local_deploy.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Step 1: Build the binary
+echo "Building the application..."
+go build -o flight-prices-binary
+
+# Check for successful build
+if [ $? -ne 0 ]; then
+    echo "Build failed. Exiting."
+    exit 1
+fi
+
+# Step 2: Run the binary
+echo "Starting the application..."
+./flight-prices-binary &
+
+# Allow some time for the server to start
+sleep 2
+
+# Step 3: Open a web browser
+echo "Opening web browser..."
+case "$OSTYPE" in
+  darwin*) open "http://localhost:8080" ;; 
+  linux*) xdg-open "http://localhost:8080" ;;
+  *) echo "Unknown OS type. Please open a web browser manually at http://localhost:8080" ;;
+esac
+
+exit 0
