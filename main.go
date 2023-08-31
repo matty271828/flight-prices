6dc9c1a9b65e38d8a8f4a11c80688670087fb7ec
MATTHEW MacLean <mrm109@cs.bham.ac.uk>
2023-09-01 00:45:20 +0100

    Add cachebusting to UI

--- main.go
+++ main.go
@@ -2,10 +2,13 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
 	"path/filepath"
+	"strings"
+	"time"
 
 	"github.com/matty271828/flight-prices/amadeus"
 	"github.com/matty271828/flight-prices/controller"
@@ -33,11 +36,43 @@ func main() {
 	mux := http.NewServeMux()
 	mux.Handle("/api/", http.StripPrefix("/api", s))
 
+	// Serve index.html dynamically, this is for cachebusting
+	mux.HandleFunc("/", indexHandler)
+
 	uiPath := filepath.Join(basepath, "ui")
 	log.Printf("Serving UI from: %s", uiPath) // Log the UI path for debugging
+
+	// Serve other static files
 	fs := http.FileServer(http.Dir(uiPath))
-	mux.Handle("/", fs)
+	mux.Handle("/static/", http.StripPrefix("/static", fs))
 
 	// Start the server
 	log.Fatal(http.ListenAndServe(":8080", mux))
+}
+
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
+	modifiedContent, err := generateHTML()
+	if err != nil {
+		http.Error(w, "Failed to generate content", http.StatusInternalServerError)
+		return
+	}
+	w.Write([]byte(modifiedContent))
+}
+
+func generateHTML() (string, error) {
+	content, err := ioutil.ReadFile("ui/index.html")
+	if err != nil {
+		return "", err
+	}
+
+	timestamp := fmt.Sprintf("%d", time.Now().Unix())
+	// Add timestamps for cachebusting
+	modifiedContent := strings.ReplaceAll(string(content), "{{cssTimestamp}}", timestamp)
+	modifiedContent = strings.ReplaceAll(modifiedContent, "{{jsTimestamp}}", timestamp)
+
+	return modifiedContent, nil
 }
