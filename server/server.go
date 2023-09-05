dd02e33cfccb8fa702c9b3578bfb341774a142e7
MATTHEW MacLean <mrm109@cs.bham.ac.uk>
2023-09-05 01:10:10 +0100

    Extract server setup to server package

--- server/server.go
+++ server/server.go
@@ -3,8 +3,12 @@ package server
 import (
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
+	"path/filepath"
+	"strings"
+	"time"
 
 	"github.com/gorilla/mux"
 	"github.com/matty271828/flight-prices/controller"
@@ -13,6 +17,10 @@ import (
 type Server struct {
 	ControllerManager controller.ControllerManager
 	Router            *mux.Router
+	UIBasepath        string
+	UIType            string
+	Route             string
+	Port              string
 }
 
 func NewServer(c controller.ControllerManager) *Server {
@@ -23,11 +31,65 @@ func NewServer(c controller.ControllerManager) *Server {
 	return server
 }
 
-// SetupRoutes is called when we want to include
-// the api on an initialised server instance.
+// SetupRoutes is called when we want to include the API on an initialised server instance.
 func (s *Server) SetupRoutes() {
 	apiRouter := s.Router.PathPrefix("/api").Subrouter()
 	apiRouter.HandleFunc("/get-destinations/", s.HandleGetDestinations).Methods("GET")
+}
+
+func (s *Server) Start(basepath, uiType, route, port string) {
+	s.UIBasepath = basepath
+	s.UIType = uiType
+	s.Route = route
+	s.Port = port
+
+	s.setupUIRoutes()
+	s.setupServer()
+}
+
+func (s *Server) setupUIRoutes() {
+	// Serve the index.html dynamically for cachebusting
+	s.Router.HandleFunc("/", s.serveIndex())
+
+	// Serve other static files
+	dirPath := filepath.Join(s.UIBasepath, s.UIType)
+	fs := http.FileServer(http.Dir(dirPath))
+	s.Router.PathPrefix(s.Route).Handler(http.StripPrefix(s.Route, fs))
+}
+
+func (s *Server) setupServer() {
+	log.Printf("HTTP Server initialized on port %s", s.Port)
+	log.Fatal(http.ListenAndServe(":"+s.Port, s.Router))
+}
+
+func (s *Server) serveIndex() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		if r.URL.Path != "/" {
+			http.NotFound(w, r)
+			return
+		}
+
+		modifiedContent, err := generateHTML(filepath.Join(s.UIType, "index.html"))
+		if err != nil {
+			http.Error(w, "Failed to generate content", http.StatusInternalServerError)
+			return
+		}
+		w.Write([]byte(modifiedContent))
+	}
+}
+
+func generateHTML(filepath string) (string, error) {
+	content, err := ioutil.ReadFile(filepath)
+	if err != nil {
+		return "", err
+	}
+
+	timestamp := fmt.Sprintf("%d", time.Now().Unix())
+	// Add timestamps for cachebusting
+	modifiedContent := strings.ReplaceAll(string(content), "{{cssTimestamp}}", timestamp)
+	modifiedContent = strings.ReplaceAll(modifiedContent, "{{jsTimestamp}}", timestamp)
+
+	return modifiedContent, nil
 }
 
 func (s *Server) HandleGetDestinations(w http.ResponseWriter, r *http.Request) {
