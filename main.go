0db608eebcdd11f318611b47a4956ae4df845673
MATTHEW MacLean <mrm109@cs.bham.ac.uk>
2023-09-05 01:10:10 +0100

    Extract server setup to server package

--- main.go
+++ main.go
@@ -2,14 +2,10 @@ package main
 
 import (
 	"fmt"
-	"io/ioutil"
 	"log"
-	"net/http"
 	"os"
 	"path/filepath"
-	"strings"
 	"sync"
-	"time"
 
 	"github.com/joho/godotenv"
 	"github.com/matty271828/flight-prices/amadeus"
@@ -52,68 +48,26 @@ func setupEnv() string {
 	return filepath.Dir(execPath)
 }
 
-func setupServers(basepath string, c *controller.Controller, wg *sync.WaitGroup) {
+func setupServers(basepath string, c controller.ControllerManager, wg *sync.WaitGroup) {
+	// Init the first server
 	s := server.NewServer(c)
-	devS := server.NewServer(c)
-	devS.SetupRoutes()
+	s.SetupRoutes()
 
 	// Start UI Handler
 	wg.Add(1)
-	go startServer(basepath, "ui", "/static/", "8080", s, wg)
+	go func() {
+		defer wg.Done()
+		s.Start(basepath, "ui", "/static/", "8080")
+	}()
+
+	// Init the dev server
+	devS := server.NewServer(c)
+	devS.SetupRoutes()
 
 	// Start Dev UI Handler
 	wg.Add(1)
-	go startServer(basepath, "ui-dev", "/devstatic/", "8091", devS, wg)
-}
-
-func startServer(basepath, uiType, route, port string, s *server.Server, wg *sync.WaitGroup) {
-	defer wg.Done()
-	setupServer(basepath, uiType, route, port, s)
-}
-
-// setupServer is used to setup a server on a requested port with a supplied ui
-func setupServer(basepath, dir, staticRoute, port string, s *server.Server) {
-	r := s.Router
-
-	// Serve the index.html dynamically for cachebusting
-	r.HandleFunc("/", serveIndexFromDir(dir))
-
-	// Serve other static files
-	dirPath := filepath.Join(basepath, dir)
-	fs := http.FileServer(http.Dir(dirPath))
-	r.PathPrefix(staticRoute).Handler(http.StripPrefix(staticRoute, fs))
-
-	log.Printf("HTTP Server initialized on port %s", port)
-	log.Fatal(http.ListenAndServe(":"+port, r))
-}
-
-// serveIndexFromDir is used to serve the index file of a ui directory
-func serveIndexFromDir(dirPath string) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		if r.URL.Path != "/" {
-			http.NotFound(w, r)
-			return
-		}
-
-		modifiedContent, err := generateHTML(filepath.Join(dirPath, "index.html"))
-		if err != nil {
-			http.Error(w, "Failed to generate content", http.StatusInternalServerError)
-			return
-		}
-		w.Write([]byte(modifiedContent))
-	}
-}
-
-func generateHTML(filepath string) (string, error) {
-	content, err := ioutil.ReadFile(filepath)
-	if err != nil {
-		return "", err
-	}
-
-	timestamp := fmt.Sprintf("%d", time.Now().Unix())
-	// Add timestamps for cachebusting
-	modifiedContent := strings.ReplaceAll(string(content), "{{cssTimestamp}}", timestamp)
-	modifiedContent = strings.ReplaceAll(modifiedContent, "{{jsTimestamp}}", timestamp)
-
-	return modifiedContent, nil
+	go func() {
+		defer wg.Done()
+		devS.Start(basepath, "ui-dev", "/devstatic/", "8091")
+	}()
 }
